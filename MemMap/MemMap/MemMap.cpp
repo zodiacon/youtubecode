52c7d84f44b4ec947bb718dbb89504d91c83297c
Pavel Yosifovich <zodiacon@live.com>
2026-08-30 12:47:35 -0400

    Code additions for part 3

--- MemMap/MemMap/MemMap.cpp
+++ MemMap/MemMap/MemMap.cpp
@@ -1,15 +1,62 @@
 // MemMap.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <Windows.h>
+#include <phnt_windows.h>
+#include <phnt.h>
 #include <stdio.h>
 #include <string>
-#include <winternl.h>
 #include <format>
 #include <Psapi.h>
+#include <TlHelp32.h>
 #include <map>
+#include <vector>
 
 #pragma comment(lib, "ntdll")
+
+struct ThreadInfo {
+	DWORD Id;
+	PVOID Teb;
+	PVOID StackStart, StackEnd;
+};
+
+ThreadInfo GetThreadInfo(DWORD id, HANDLE hProcess) {
+	ThreadInfo ti{};
+	ti.Id = id;
+
+	if (auto hThread = OpenThread(THREAD_QUERY_INFORMATION, FALSE, id); hThread != nullptr) {
+		THREAD_BASIC_INFORMATION tbi;
+		if (0 == NtQueryInformationThread(hThread, ThreadBasicInformation, &tbi, sizeof(tbi), nullptr)) {
+			ti.Teb = tbi.TebBaseAddress;
+			NT_TIB tib;
+			if (ReadProcessMemory(hProcess, ti.Teb, &tib, sizeof(tib), nullptr)) {
+				ti.StackStart = tib.StackBase;
+				ti.StackEnd = tib.StackLimit;
+			}
+		}
+		CloseHandle(hThread);
+	}
+	return ti;
+}
+
+std::vector<ThreadInfo> EnumThreads(HANDLE hProcess) {
+	auto hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, 0);
+	if (hSnapshot == INVALID_HANDLE_VALUE)
+		return {};
+
+	THREADENTRY32 te;
+	te.dwSize = sizeof(te);
+	Thread32First(hSnapshot, &te);
+	auto pid = GetProcessId(hProcess);
+
+	std::vector<ThreadInfo> threads;
+	while (Thread32Next(hSnapshot, &te)) {
+		if (te.th32OwnerProcessID == pid) {
+			threads.push_back(GetThreadInfo(te.th32ThreadID, hProcess));
+		}
+	}
+
+	return threads;
+}
 
 const char* GetBlockState(DWORD state) {
 	switch (state) {
@@ -85,22 +132,33 @@ std::string GetBlockDetails(HANDLE hProcess, MEMORY_BASIC_INFORMATION const& mbi
 	if (mbi.State != MEM_COMMIT)
 		return "";
 
-	std::string details;
-	static PROCESS_BASIC_INFORMATION pbi;
-	if (pbi.PebBaseAddress == nullptr) {
-		NtQueryInformationProcess(hProcess, ProcessBasicInformation, &pbi, sizeof(pbi), nullptr);
-	}
-	if (pbi.PebBaseAddress && pbi.PebBaseAddress >= mbi.BaseAddress && (PBYTE)pbi.PebBaseAddress < (PBYTE)mbi.BaseAddress + mbi.RegionSize) {
-		details = std::format("PEB (0x{})", (PVOID)pbi.PebBaseAddress);
-		return details;
-	}
+	static auto threads = EnumThreads(hProcess);
 
-	if (mbi.Type == MEM_MAPPED || mbi.Type == MEM_IMAGE) {
+	std::string details;
+	if (mbi.Type == MEM_PRIVATE) {
+		static PROCESS_BASIC_INFORMATION pbi;
+		if (pbi.PebBaseAddress == nullptr) {
+			NtQueryInformationProcess(hProcess, ProcessBasicInformation, &pbi, sizeof(pbi), nullptr);
+		}
+		if (pbi.PebBaseAddress && pbi.PebBaseAddress >= mbi.BaseAddress && (PBYTE)pbi.PebBaseAddress < (PBYTE)mbi.BaseAddress + mbi.RegionSize) {
+			details = std::format("PEB (0x{})", (PVOID)pbi.PebBaseAddress);
+			return details;
+		}
+
+		for (auto& t : threads) {
+			if (t.Teb && mbi.BaseAddress <= t.Teb && t.Teb < (PBYTE)mbi.BaseAddress + mbi.RegionSize) {
+				return std::format("TEB (0x{}) ID: {}", t.Teb, t.Id);
+			}
+			if (t.StackEnd && mbi.BaseAddress >= t.StackEnd && mbi.BaseAddress < t.StackStart) {
+				return std::format("Stack ID: {}", t.Id);
+			}
+		}
+	}
+	else {
 		char path[MAX_PATH];
 		if (GetMappedFileNameA(hProcess, mbi.BaseAddress, path, _countof(path)))
 			return NtPathToDosPath(path);
 	}
-
 	return details;
 }
 
@@ -111,7 +169,7 @@ int main(int argc, const char* argv[]) {
 	}
 
 	auto pid = strtoul(argv[1], nullptr, 0);
-	HANDLE hProcess = OpenProcess(PROCESS_QUERY_INFORMATION, FALSE, pid);
+	HANDLE hProcess = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, pid);
 	if (!hProcess) {
 		printf("Error: %u\n", GetLastError());
 		return 1;
